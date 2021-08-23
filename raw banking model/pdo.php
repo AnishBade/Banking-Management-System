4dcefc4054cf88772f432b761fd523c438adf61a
AnishBade <badeanish6045@gmail.com>
2021-08-23 22:44:15 +0545

    raw model

---
+++ raw banking model/pdo.php
@@ -0,0 +1,5 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=bade_dbms','anish','bade');
+// $pdo =new PDO('mysql:host=localhost;dbname=bade_dbms','','');t=localhost;port=3306;dbname=bade
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+?>
